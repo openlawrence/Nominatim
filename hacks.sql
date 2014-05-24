4f320fa6f7011f123d1ec05806d3c837202a5a1f
James Michael DuPont <JamesMikeDuPont@gmail.com>
2014-05-24 08:40:56 -0500

    search cli

--- hacks.sql
+++ hacks.sql
@@ -1,3 +1,16 @@
 ; nominclature for lawrence
-insert into word (word_token, word) values('mass','Massachusetts Street');
-insert into word (word_token, word) values('mass st','Massachusetts Street');
+select getorcreate_word_id('mass');
+select getorcreate_word_id('mass st');
+
+select word_id  from word where word_token = 'mass';
+
+#update search_name set name_vector = (name_vector + where nameaddress_vector @> ARRAY[
+
+
+select word_id, word, word_token  from word where word_token like 'mass%' ;
+ word_id | word |         word_token         
+   12980 |      | mass
+  137723 |      | mass st
+
+select * from search_name where name_vector @> ARRAY[12980];
+
